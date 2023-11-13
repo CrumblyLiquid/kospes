c1460006d0223bfa3985ea6c3e8714f1bed28a9f
CrumblyLiquid <crumblyliquid@gmail.com>
2023-11-13 15:35:23 +0100

    Implement notifications

--- src/main.py
+++ src/main.py
@@ -1,19 +1,49 @@
 import discord
 from discord.ext import tasks
 
+from typing import Optional, Union
+
+from datetime import datetime
 from dotenv import load_dotenv
 from os import getenv
 
-from sirius import SiriusAPI
+from sirius import SiriusAPI, EventType
 from config import Config
 
 class KOSPes(discord.Client):
     config: Config
     sirius: SiriusAPI
 
+    async def send_notification(self, course: str, event):
+        for channel_id in self.config.channels:
+            channel: Optional[Union[discord.abc.GuildChannel, discord.Thread, discord.abc.PrivateChannel]] = self.get_channel(channel_id)
+            if channel is None:
+                channel = await self.fetch_channel(channel_id)
+            if(type(channel) is discord.TextChannel):
+                await channel.send(f"[{course}] Nový termín: {event['id']}")
+
+    @tasks.loop(hours=2)
+    async def update(self):
+        for course in self.config.courses:
+            info = await self.sirius.course_events(
+                course,
+                event_type = EventType.Assessment,
+                start = datetime.now()
+            )
+
+            if(info is not None and "events" in info):
+                for event in info["events"]:
+                    if "id" in event:
+                        if event["id"] not in self.config.seen_events:
+                            self.config.seen_events.append(event["id"])
+                            await self.send_notification(course, event)
+
+        self.config.save()
+
     async def on_ready(self):
         print(f"Logged in as {self.user}")
-        print("Starting loops")
+        self.update.start()
+        print("Update loop started!")
 
 if __name__ == "__main__":
     load_dotenv()
