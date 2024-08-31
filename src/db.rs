130e2786c1ed4be010a753121a38cd3b29e84bec
CrumblyLiquid <crumblyliquid@gmail.com>
2024-08-31 21:27:10 +0200

    Check if database exists

--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,13 @@
+use std::fs;
 use std::path::PathBuf;
 
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePool};
 
 pub async fn get_db(path: &PathBuf) -> SqlitePool {
+    if !path.try_exists().unwrap_or(false) {
+        panic!("Database {:?} doesn't exist!", path);
+    }
+
     // Create SQLite database connection
     // Used for storing seen events, news, etc.
     let db_options = SqliteConnectOptions::new()
@@ -12,6 +17,8 @@ pub async fn get_db(path: &PathBuf) -> SqlitePool {
     let db_pool = SqlitePool::connect_with(db_options)
         .await
         .expect("Failed to connect to the SQLite database");
+
+    println!("Connected to {:?}", fs::canonicalize(path));
 
     db_pool
 }
